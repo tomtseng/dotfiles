62b69d0d0441cfc285a3b4862561c9a5e8ea411b
Tom Tseng <tom.hm.tseng@gmail.com>
2019-10-24 17:00:16 -0400

    install: Don't source zshrc
    It messes with the flow of the installation when sourced midway through.

--- install.sh
+++ install.sh
@@ -65,8 +65,6 @@ cp .oh-my-zsh/themes/* ~/.oh-my-zsh/themes/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-source ~/.zshrc
-
 ############
 ## Vim setup
 ############
