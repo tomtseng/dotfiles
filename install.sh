3dc4fd321542dc655388b11c9b4a6286844309e7
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 11:21:18 -0500

    install: Don't launch zsh on installing oh-my-zsh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ elif [ ${os} == "linux" ]; then
 fi
 chsh -s $(which zsh)
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash -c "$(RUNZSH=no curl --fail --silent --show-error --location https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
 cp .oh-my-zsh/themes/* ~/.oh-my-zsh/themes/
 
