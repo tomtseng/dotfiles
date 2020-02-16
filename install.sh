4eb0c8386462a492888023db4270c97edc1d5a4b
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-16 01:12:08 -0500

    install: Remove fzf install prompts

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ fi
 ###########
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --key-bindings --completion --no-update-rc
 
 ###########
 # ripgrep setup
